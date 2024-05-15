2eafe8a4aff2b162fe8f02b560265596334859e1
rajpatel2435 <rajpatel2435@gmail.com>
2024-05-14 23:01:09 -0400

    feat: db connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+ import mongoose from "mongoose";
+
+ type ConnectionObject={
+    isConnected? :number,
+ }
+
+ const connection: ConnectionObject= {};
+
+ async function dbConnect():Promise<void>{
+    if(connection.isConnected){
+        console.log("already connected no need to re connect");
+        return
+    }
+
+    try{
+const db=await mongoose.connect(process.env.MONGODB_URI || '',{});
+
+        connection.isConnected =db.connections[0].readyState;
+    }catch (error){
+        console.log("errororororororor");
+        console.error(error);
+        process.exit(1);
+        // exit if it has error
+    }
+ }
+
+
+ export default dbConnect;
